fe536340992491c47255b2a1dace051aceb19093
sauheiwong <sauheiwong@gmail.com>
2025-09-28 20:48:14 -0700

    edit triggerSummarization tool: send a message to game room after summary

--- tools/COC/triggerSummarizationTool.js
+++ tools/COC/triggerSummarizationTool.js
@@ -1,12 +1,10 @@
-// import { GoogleGenerativeAI } from "@google/generative-ai";
+// Import the socket.io instance for real-time communication with clients.
+import { io } from "../../app.js";
 import { Type, GoogleGenAI } from "@google/genai";
 
 import Game from "../../models/gameModel.js";
 import COCCharacterModel from "../../models/COCCharacterModel.js";
-import messageModel from "../../models/messageModel.js";
-import { sum } from "mathjs";
 
-// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API });
 
 const buffer = 10;
@@ -37,14 +35,6 @@ const triggerSummarization = async(game, messages) => {
     你是一個TRPG遊戲的紀錄員。請閱讀以下舊的摘要和完整的對話歷史，生成一段新的、精簡的、涵蓋所有關鍵劇情點的摘要，要包括故事的目標、地點、時代、NPC、NPC們與主角的關係和名字。不需要包含主角資料，因為KP有主角的角色卡。
     `
 
-    // const sumarizerModel = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-
-    // const chat = sumarizerModel.startChat({
-    //     history: [{ role: "user", parts: [{ text: summaryPrompt }]}],
-    // })
-    // const result = await chat.sendMessage(contentToSummarize);
-    // const newSummary = result.response.text();
-
     const result = await ai.models.generateContent({
         model: "gemini-2.5-flash",
         contents: [{ role: "user", parts: [{ text: contentToSummarize }]}],
@@ -74,9 +64,9 @@ const triggerSummarization = async(game, messages) => {
         }
     });
 
-    console.log(`game ${game._id} summrization is: ${newSummary}`);
-
     console.log(`game ${game._id} summrization success. New anchor is at index ${newAnchorIndex}`)
+
+    io.to(game._id).emit("systemMessage:received", { message: "New summary have been created" })
     return;
 }
 
